678d11a6044cd926c2132fa5a2652ff9e3821e68
Brandon Vargas <117324772+vistruth@users.noreply.github.com>
2024-07-30 11:18:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import time
 
 
 
-
-
+#This function will first check if theres a keys.py file with the credentials needed to make the API calls.
 def secret_check():
     if os.path.isfile("IgnoreFolder/keys.py"):
         main_menu()
+
+    #if it doesnt have the IgnoreFolder directory it'll create one 
     elif not os.path.isdir("IgnoreFolder"):
         print("no direct")
         os.mkdir("IgnoreFolder")
         secret_check()
+
+    #now it'll ask the user to input their api credentials then creates a files to use 
     else:
         print("\n\nSeems like you have some missing information.\n\n\nPlease enter the values for the following...\n\n")
         f = open("IgnoreFolder/keys.py", "a")
@@ -42,9 +45,8 @@ def main_menu():
         print("2 - Get Bypass codes")
         print("3 - Delete bypass codes")
         print("4 - Update alias")
-        print("5 - User creation")
-        print("6 - User deletion")
-        print("7 - Exit")
+        print("5 - Test features")
+        print("6 - Exit")
         option = int(input("(Use numbers only) Enter number here: "))
         if option == 1:
             import getUserInfo
@@ -55,16 +57,33 @@ def main_menu():
         elif option == 4:
             import updateAlias
         elif option == 5:
-            import userCreation
+            test_features()
         elif option == 6:
-            import userDeletion
-        elif option == 7:
             print("Application will exit in 5 seconds, Thanks")
             time.sleep(5)
             sys.exit()
         else:
             print("This option isn't available, redirecting back to the main menu.\n\n")
             main_menu()
+# seperated this section to avoid accidental account creation/deletion
+def test_features():
+        print("These are API calls used for testing purposes.")
+        option = 0
+        while option == 0:
+            print("1 - User creation")
+            print("2 - User deletion")
+            print("3 - Return to the main menu")
+            if option == 1:
+                import userCreation
+            elif option == 2:
+                import userDeletion
+            elif option == 3:
+                print("Redirecting back to the main menu. in 5 seconds, Thanks")
+                time.sleep(5)
+                main_menu()
+            else:
+                print("This option isn't available, redirecting back to the main menu.\n\n")
+                main_menu()
 def exit():
     option = 0
     while option == 0:
